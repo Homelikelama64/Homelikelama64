e914c5ab580fdaab1daa57acb4a8c4a11c3b02f6
Homelikelama64 <homelikelama64@gmail.com>
2022-10-23 21:36:35 +1300

    waves slowy speed up

--- src/waves.js
+++ src/waves.js
@@ -1,20 +1,27 @@
 let v1Timer = 0;
 let v2Timer = 0;
 
+let v1DelayTimer = 6;
+let v2DelayTimer = 23;
+
 function resetWaves() {
     v1Timer = 0;
+    v1DelayTimer = 6;
     v2Timer = 0;
+    v2DelayTimer = 23;
 }
 
 function waves(ts) {
     v1Timer += ts;
-    while (v1Timer >= 6) {
-        v1Timer -= 6;
+    while (v1Timer >= v1DelayTimer) {
+        v1Timer -= v1DelayTimer;
         spawnMissileV1();
+        v1DelayTimer = max(v1DelayTimer - 0.07, 2);
     }
     v2Timer += ts;
     while (v2Timer >= 23) {
-        v2Timer -= 23;
-        spawnMissileV2();
+        v2Timer -= v2DelayTimer;
+        spawnMissileV1();
+        v2DelayTimer = max(v2DelayTimer - 0.05, 6);
     }
 }
