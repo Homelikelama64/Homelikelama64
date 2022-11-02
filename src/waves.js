008754e4bfe7d1a37debd8ddac830392c002b162
Homelikelama64 <homelikelama64@gmail.com>
2022-11-03 10:24:29 +1300

    made waves slightly harder

--- src/waves.js
+++ src/waves.js
@@ -32,18 +32,18 @@ function waves(ts) {
     while (v1Timer >= v1DelayTimer) {
         v1Timer -= v1DelayTimer;
         spawnMissileV1();
-        v1DelayTimer = max(v1DelayTimer - 0.07, 2);
+        v1DelayTimer = max(v1DelayTimer - 0.1, 2);
     }
     v2Timer += ts;
     while (v2Timer >= v2DelayTimer) {
         v2Timer -= v2DelayTimer;
         spawnMissileV2();
-        v2DelayTimer = max(v2DelayTimer - 0.05, 6);
+        v2DelayTimer = max(v2DelayTimer - 0.08, 6);
     }
     v3Timer += ts;
     while (v3Timer >= v3DelayTimer) {
         v3Timer -= v3DelayTimer;
         spawnMissileV3();
-        v3DelayTimer = max(v3DelayTimer - 3, 45);
+        v3DelayTimer = max(v3DelayTimer - 5, 45);
     }
 }
